9812f9c3211f3451488934a2012b73bd0b17e58c
Mostafa Khamis Hassan <69043285+mostafa-khamis@users.noreply.github.com>
2023-11-28 15:43:12 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,14 +1,14 @@
 
 // task 1 
-// var fName = prompt("Enter your first name");
-// var lName = prompt("Enter your last name");
+var fName = prompt("Enter your first name");
+var lName = prompt("Enter your last name");
 
 
-// confirm("Is your name: " + fName + " "+ lName);
+ confirm("Is your name: " + fName + " "+ lName);
 
-// var byear = Number(prompt("Enter your birth year"));
+ var byear = Number(prompt("Enter your birth year"));
 
-// document.write("Welcome " + fName + " " + lName + " your are " + (2023 - byear) + " years old" );
+ document.write("Welcome " + fName + " " + lName + " your are " + (2023 - byear) + " years old" );
 
 // task 2
 
